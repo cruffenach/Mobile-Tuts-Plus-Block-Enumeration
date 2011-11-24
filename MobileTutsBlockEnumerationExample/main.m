5bbdcab7382c43a15b718d27e2def15f5ca130ac
Collin Ruffenach <cruffenach@RuffRock.local>
2011-11-24 07:30:23 -0800

    Initial Commit

---
+++ MobileTutsBlockEnumerationExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MobileTutsBlockEnumerationExample
+//
+//  Created by Collin Ruffenach on 11/24/11.
+//  Copyright (c) 2011 ELC Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
