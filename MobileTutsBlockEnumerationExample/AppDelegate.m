2934b89633c926ad72dc7aea453196f89da4dc7f
Collin Ruffenach <cruffenach@RuffRock.local>
2011-11-30 10:10:17 -0800

    Updating README

--- MobileTutsBlockEnumerationExample/AppDelegate.m
+++ MobileTutsBlockEnumerationExample/AppDelegate.m
@@ -12,6 +12,43 @@
 
 @synthesize window = _window;
 
+- (void)enumerationExamples {
+    
+    NSDictionary *sampleDictionary = [NSDictionary dictionaryWithObjectsAndKeys:@"Hello World", @"Test String 1",
+                                      @"How are you?", @"Test String 2",
+                                      @"I am fine.", @"Test String 3",
+                                      @"Let's have lunch!", @"Test String 4",
+                                      @"Open the pod bay doors HAL.", @"Test String 5",
+                                      @"I am fine.", @"Test String 6",
+                                      nil];
+    
+    NSLog(@"Printing my dictionary out by enumerating:");
+    [sampleDictionary enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        NSLog(@"\tKey: %@ Value: %@", key, obj);
+    }];
+    
+    NSArray *sampleArray = [sampleDictionary allKeys];
+    NSLog(@"Printing my array out by enumerating:");
+    [sampleArray enumerateObjectsWithOptions:NSEnumerationReverse usingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        NSLog(@"\tValue: %@ Index: %d", obj, idx);
+    }];
+    
+    NSSet *sampleSet = [NSSet setWithArray:[sampleDictionary allValues]];
+    NSLog(@"Printing my set out by enumerating, this set will be all UNIQUE objects:");
+    [sampleSet enumerateObjectsUsingBlock:^(id obj, BOOL *stop) {
+        NSLog(@"\tValue: %@", obj);
+    }];
+    
+    NSLog(@"Printing my array out by enumerating and stopping at index 1:");
+    [sampleArray enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        NSLog(@"\tValue: %@ Index: %d", obj, idx);
+        
+        if(idx == 1) {
+            *stop = YES;
+        }
+    }];
+}
+
 - (void)dealloc
 {
     [_window release];
@@ -20,6 +57,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [self enumerationExamples];
+    
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
